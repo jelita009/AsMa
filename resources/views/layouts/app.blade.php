5fe224e7359a1af99b33933c480184aa641dd5ac
Mijaenn <hamizanputra321@gmail.com>
2025-11-20 13:45:58 +0700

    apus ignore

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,32 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
 </head>
-<style>
-    body {
-        font-family: Arial, sans-serif;
-        margin: 0;
-        padding: 0;
-        background-color: #f4f4f4;
-    }
-
-    header {
-        background-color: #35424a;
-        color: #ffffff;
-        padding: 10px 0;
-        text-align: center;
-    }
-    footer {
-        background-color: #35424a;
-        color: #ffffff;
-        text-align: center;
-        padding: 10px 0;
-        position: fixed;
-        bottom: 0;
-        width: 100%;
-    }
-</style>
 <body>
 
     {{-- <header>
